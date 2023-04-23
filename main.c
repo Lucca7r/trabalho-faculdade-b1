771074d0168d254c7df34f3265a946e37f1523e9
Lucas <114083832+Lucca7r@users.noreply.github.com>
2023-04-23 18:22:04 +0000

    update-comentarios-no-codigo

--- main.c
+++ main.c
@@ -2,6 +2,7 @@
 #include <string.h>
 #include <time.h>
 
+// Definir a estrutura da tarefas
 struct tarefa {
   char nome[100];
   char dataInicio[11];
@@ -10,19 +11,20 @@ struct tarefa {
 };
 
 struct tarefa listaTarefas[100];
-int numTarefas = 0; // numero de tarefas atual
+int numTarefas = 0; // Numero de tarefas atual
 
 void adicionarTarefa() {
   struct tarefa novaTarefa;
   int x;
   printf("\nDigite o nome da tarefa: ");
   scanf("%[^\n]", novaTarefa.nome);
-  getchar();
+  getchar(); // É usado para limpar o buffer após a leitura
 
+  // Exibir opção de inserir data inicial
   printf("\nDeseja adicionar a data de hoje ou quer inserir manualmente?\n");
   puts("\n(1) para de hoje\n(2) para manualmente: ");
   scanf("%d", &x);
-  getchar();
+  getchar(); //é usado para limpar o buffer após a leitura
 
   if (x == 1) {
     time_t t = time(NULL);         // Data atual em segundos
@@ -35,7 +37,8 @@ void adicionarTarefa() {
 
     printf("\n       😄Tarefa criada com sucesso!!😄");
   }
-  // neste bloco usuaria digitara manualmente
+  
+  // Neste bloco usuaria digitara manualmente
   else if (x == 2) {
     printf("\nDigite a nova data de início (formato dd/mm/aaaa): ");
     scanf("%s", novaTarefa.dataInicio);
@@ -44,22 +47,22 @@ void adicionarTarefa() {
 
     printf("\n       😄Atividade criada com sucesso!!😄");
   }
-
+  // Ter o controle de tarefas adicionadas na estrutura
   listaTarefas[numTarefas] = novaTarefa;
   numTarefas++;
 }
 
 void editarTarefa() {
   char nomeTarefa[100];
+  int encontrou = 0; // Flag para indicar se a tarefa foi encontrada ou não
 
   printf("\nDigite o nome da tarefa que deseja editar: \n");
   scanf("%[^\n]", nomeTarefa);
   getchar();
 
   for (int i = 0; i < numTarefas; i++) {
-    if (strcmp(listaTarefas[i].nome, nomeTarefa) ==
-        0) { // aqui ele vai compara a nome com o nome de todas tarefas salvas
-
+    // Aqui ele vai compara a nome digiado com o nome de todas tarefas salvas
+    if (strcmp(listaTarefas[i].nome, nomeTarefa) == 0) {
       printf("\nDigite o novo nome da tarefa: ");
       scanf("%[^\n]", listaTarefas[i].nome);
       getchar();
@@ -74,15 +77,21 @@ void editarTarefa() {
       scanf("%s", listaTarefas[i].status);
 
       printf("\n       😄Atualizado com sucesso!!😄");
-      return;
+
+      encontrou = 1;
+      break;
     }
+  } // caso tarefa nao encontrada
+  if (!encontrou) {
     printf("\n       😐Tarefa não encontrada😐\n");
   }
 }
 
 void buscarTarefa() {
   char nomeTarefa[100];
+  int encontrou = 0; // Flag para indicar se a tarefa foi encontrada ou não
 
+  // Buscar nome na lista de tarefas
   printf("\nDigite o nome da tarefa que deseja buscar: ");
   scanf("%[^\n]", nomeTarefa);
   getchar();
@@ -94,10 +103,15 @@ void buscarTarefa() {
       printf("Data de inicio: %s\n", listaTarefas[i].dataInicio);
       printf("Data de término: %s\n", listaTarefas[i].dataTermino);
       printf("Status: %s\n", listaTarefas[i].status);
-      return;
-    }else {
-      printf("\n       😐Tarefa não encontrada😐\n");
+
+      // Seta a flag como verdadeira
+      encontrou = 1;
+      break;
     }
+  }
+  // Caso tarefa nao encontrada
+  if (!encontrou) {
+    printf("\n       😐Tarefa não encontrada😐\n");
   }
 }
 
@@ -106,10 +120,12 @@ void mudarStatus() {
   int posicao = -1;
   int update = 0;
 
+  // Solicitar nome da tarefa
   printf("Digite o nome da tarefa que seja mudar o status: ");
   scanf("%[^\n]", nomeTarefa);
   getchar();
 
+  // Buscar nome na lista de tarefas
   for (int i = 0; i < numTarefas; i++) {
     if (strcmp(listaTarefas[i].nome, nomeTarefa) == 0) {
       posicao = i;
@@ -117,15 +133,18 @@ void mudarStatus() {
     }
   }
 
+  // Exibe as opções de atualização do status
   printf("\n(1) Atualizar status para concluido automatico \n");
   printf("\n(2) Atualizar status manualmente \n");
   scanf("%d", &update);
   getchar();
 
+  // Verifica se a opção de atualização é válida
   if (update != 1 && update != 2) {
     printf("\n       😐Decisão invalidade!😐");
   }
 
+  // Atualiza o status para "Concluído" automaticamente
   if (update == 1) {
 
     time_t t = time(NULL);
@@ -137,6 +156,7 @@ void mudarStatus() {
     printf("\n       😄Atualizado com sucesso!!😄");
   }
 
+  // Atualiza o status manualmente
   else if (update == 2) {
 
     printf("\nDigite a nova data de término (formato dd/mm/aaaa): ");
@@ -155,7 +175,7 @@ void removerTarefa() {
   int posicao = -1;
   printf("\nDigite o nome da terafa que deseja excluir: ");
   scanf("%s", nomeTarefa);
-  getchar();
+  getchar(); //é usado para limpar o buffer após a leitura
 
   for (int i = 0; i < numTarefas; i++) {
     if (strcmp(listaTarefas[i].nome, nomeTarefa) == 0) {
@@ -198,7 +218,7 @@ int main(void) {
     puts("(5)Remover ❌");
     puts("(6)Quantidade de tarefas atualmente🗂");
     scanf("%d", &x);
-    getchar();
+    getchar(); //é usado para limpar o buffer após a leitura
 
     switch (x) {
     case 1:
